756470ea92c8c55b815d534faff68b3a4c35a471
2894638479 <2894638479@qq.com>
2025-03-16 20:32:46 +0800

    fix default setting value

--- src/main/java/net/fabricmc/example/KeyMapping.java
+++ src/main/java/net/fabricmc/example/KeyMapping.java
@@ -4,14 +4,14 @@ import net.minecraft.src.KeyBinding;
 import org.lwjgl.input.Keyboard;
 
 public class KeyMapping {
-    public static KeyBinding NUM1 = new KeyBinding("1",Keyboard.KEY_9);
-    public static KeyBinding NUM2 = new KeyBinding("2",Keyboard.KEY_9);
-    public static KeyBinding NUM3 = new KeyBinding("3",Keyboard.KEY_9);
-    public static KeyBinding NUM4 = new KeyBinding("4",Keyboard.KEY_9);
-    public static KeyBinding NUM5 = new KeyBinding("5",Keyboard.KEY_9);
-    public static KeyBinding NUM6 = new KeyBinding("6",Keyboard.KEY_9);
-    public static KeyBinding NUM7 = new KeyBinding("7",Keyboard.KEY_9);
-    public static KeyBinding NUM8 = new KeyBinding("8",Keyboard.KEY_9);
+    public static KeyBinding NUM1 = new KeyBinding("1",Keyboard.KEY_1);
+    public static KeyBinding NUM2 = new KeyBinding("2",Keyboard.KEY_2);
+    public static KeyBinding NUM3 = new KeyBinding("3",Keyboard.KEY_3);
+    public static KeyBinding NUM4 = new KeyBinding("4",Keyboard.KEY_4);
+    public static KeyBinding NUM5 = new KeyBinding("5",Keyboard.KEY_5);
+    public static KeyBinding NUM6 = new KeyBinding("6",Keyboard.KEY_6);
+    public static KeyBinding NUM7 = new KeyBinding("7",Keyboard.KEY_7);
+    public static KeyBinding NUM8 = new KeyBinding("8",Keyboard.KEY_8);
     public static KeyBinding NUM9 = new KeyBinding("9",Keyboard.KEY_9);
     public static KeyBinding F5 = new KeyBinding("F5", Keyboard.KEY_F5);
     public static KeyBinding getNum(int i){
